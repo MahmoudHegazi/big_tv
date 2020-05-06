64f0cd0bcd08062fbc720396ee3d6138f6faff37
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-05-07 01:10:34 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -53,6 +53,6 @@ def showAll():
     
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
+    app.secret_key = 'super_secret_key_forsideinrange(5):print(.##heLlowOrld!@#)'
     app.debug = True
     app.run(host='0.0.0.0', port=5000, threaded=False)
