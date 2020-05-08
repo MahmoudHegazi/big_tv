f4be580bfb3dfbbc439fcd7075030a4a25d3e519
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-05-08 22:00:19 +0200

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, ForeignKey, Integer, String
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, ForeignKey, Integer, String, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -89,6 +90,72 @@ class Series(Base):
         }
 
 
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    server1 = Column(String(500))
+    server2 = Column(String(500))
+    server3 = Column(String(500))
+    server4 = Column(String(500))
+    image = Column(String(500))
+    ep_number = Column(Integer)
+    tag1 = Column(String(100))
+    tag2 = Column(String(70))
+    tag3 = Column(String(70))
+    tag4 = Column(String(70))
+    tag5 = Column(String(70))
+    tag6 = Column(String(70))
+    tag7 = Column(String(70))
+    tag8 = Column(String(70))
+    tag9 = Column(String(70))
+    tag10 = Column(String(70))
+    tag11 = Column(String(70))
+    tag12 = Column(String(70))
+    tag13 = Column(String(70))
+    tag14 = Column(String(70))
+    tag15 = Column(String(70))
+    tag16 = Column(String(70))
+    tag17 = Column(String(70))
+    series_id = Column(Integer, ForeignKey('series.id'))
+    series = relationship(Series)    
+    
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'server1': self.server1,
+            'server2': self.server2,
+            'server3': self.server3,
+            'server4': self.server4,
+            'image': self.image,
+            'ep_number': self.ep_number,
+            'tag1': self.tag1,
+            'tag2': self.tag2,
+            'tag3': self.tag3,
+            'tag4': self.tag4,
+            'tag5': self.tag5,
+            'tag6': self.tag6,
+            'tag7': self.tag7,
+            'tag8': self.tag8,
+            'tag9': self.tag9,
+            'tag10': self.tag10,
+            'tag11': self.tag11,
+            'tag12': self.tag12,
+            'tag13': self.tag13,
+            'tag14': self.tag14,
+            'tag15': self.tag15,
+            'tag16': self.tag16,
+            'tag17': self.tag17,
+            'series_id': self.series_id,
+            
+        }
+        
+        
+        
 class Movie(Base):
     __tablename__ = 'movie'
 
