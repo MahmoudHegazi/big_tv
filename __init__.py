10507b491704c50011850f4b3190cbdc1d5b70d6
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-05-09 21:38:13 +0200

    update__init__.py
    added search function

--- __init__.py
+++ __init__.py
@@ -64,6 +64,34 @@ def showSeries(series_id, content_id):
     print(u'hi %s' %episode.name)
     return render_template('video.html', series = series, boots = boots, episode=episode)
 
+@app.route('/mform')
+def index():
+	return render_template('form.html')   
+
+
+    
+    
+@app.route('/process', methods=['POST'])
+def showprocess():    
+    
+    mytag = request.form['search']
+
+    if mytag:
+        myseries =  session.query(Series).filter_by(name=mytag).first()
+        mymovie =  session.query(Movie).filter_by(name=mytag).first()
+        if myseries:
+            message = '1 Result Found for %s .' %myseries.name
+            myurl = '%s/' %myseries.url
+            print(myurl)
+            return jsonify({'search' : message, 'url' : myurl})
+        if mymovie:
+            message = '1 Result Found for %s .' %mymovie.name
+            myurl = '%s/' %mymovie.url
+            return jsonify({'search' : message, 'url' : myurl})
+   
+    return jsonify({'error' : 'No Result Found!'})
+
+
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
